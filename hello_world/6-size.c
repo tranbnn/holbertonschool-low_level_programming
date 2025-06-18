ff4dbe04384cc87a607d479611d1393f40bd42d9
tranbnn <tranbn@outlook.com>
2025-06-18 12:16:16 +0000

    docs: adding 6-size

---
+++ hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * Description: main - prints variable types to stdout
+ * Return: 0 if success
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long l;
+	long long ll;
+	float f; 
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of an long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+	printf("Size of an long long int: %lu byte(s)\n", (unsigned long)sizeof(ll));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
+
+
