42b37c5aff88b9c4002c2882b863fb788f4bd4e3
tranbnn <tranbn@outlook.com>
2025-06-18 10:15:40 +0000

    docs: adding description

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main to print out string to stdout
+ * return 0 if success. 
+ *
+ */
+
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
