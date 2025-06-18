31caff18a8ae3a7a49ba5e32e373f79dbcdd33ba
tranbnn <tranbn@outlook.com>
2025-06-18 11:57:03 +0000

    docs: adding description

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ *
+ * Description: main- print string to stdout
+ * Return: 0 if success
+ *
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
