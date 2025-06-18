2dd7d937fba9dcd457f70c2a771010c045cebc14
tranbnn <tranbn@outlook.com>
2025-06-18 11:26:52 +0000

    adding 4-puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
- *main - to print out string to stdout
- *return: 0 if success. 
+ * main - to print string to stdout
+ * return 0 if success
  *
  */
 int main(void)
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
