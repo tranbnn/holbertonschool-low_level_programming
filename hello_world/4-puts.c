ab05b9ea0cec9c7f15edde5c50baa2fb1f9ba0e9
tranbnn <tranbn@outlook.com>
2025-06-18 10:26:30 +0000

    docs: whitespace

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
+#include <stdio.hii>
 /**
  *main - to print out string to stdout
  *return: 0 if success. 
  *
  */
-int main(void)
+int main (void)
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
+
 }
