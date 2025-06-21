0f409ebafa1f07fde19a1618339a928beaae42bb
tranbnn <tranbn@outlook.com>
2025-06-21 15:04:05 +0000

    fix: fixing whitespaces

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -2,10 +2,10 @@
 
 int main (void)
 {
-	
-	char letter; 
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+char letter; 
+
+	for(letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 
 	putchar('\n');
