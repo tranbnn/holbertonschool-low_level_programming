91894ce7ea216b68d451e6f1277c8105a62d4e52
tranbnn <tranbn@outlook.com>
2025-06-21 12:35:28 +0000

    fix: removing whitespace

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,15 +2,12 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
-
 /* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -18,7 +15,7 @@ int main(void)
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
-	}	
+	}
 	else
 	{
 		printf("%d is negative\n", n);
