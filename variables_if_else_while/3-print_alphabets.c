efc49acec8b5d7bdc9165065c1b8297953a10786
tranbnn <tranbn@outlook.com>
2025-06-21 15:12:50 +0000

    feat: adding 2-print

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * Description: main - prints out the alphabet in lowercase, and then uppercase to stout
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+char lower;
+char upper;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	       putchar(lower);
+
+	for (upper = 'A'; upper <= 'Z'; upper++)
+		putchar(upper);
+
+	putchar('\n');	
+
+	return (0);	
+}
