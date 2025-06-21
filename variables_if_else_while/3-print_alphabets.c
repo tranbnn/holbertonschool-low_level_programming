d4b4eca414f38ac84258bb6cda4fdbe6a3f4f7df
tranbnn <tranbn@outlook.com>
2025-06-21 15:24:20 +0000

    fix: whitespace fix

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -9,14 +9,14 @@ int main(void)
 {
 	char lower;
 	char upper;
-	
+
 	for (lower = 'a'; lower <= 'z'; lower++)
 		putchar(lower);
-	
+
 	for (upper = 'A'; upper <= 'Z'; upper++)
 		putchar(upper);
-	
+
 	putchar('\n');
-	
+
 	return (0);
 }
