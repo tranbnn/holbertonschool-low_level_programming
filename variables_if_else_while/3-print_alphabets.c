a7755a147c882fa25d7a501e91d2c9471e568c0b
tranbnn <tranbn@outlook.com>
2025-06-21 15:23:05 +0000

    fix: spacing fix

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-    char lower;
-    char upper;
-
-    for (lower = 'a'; lower <= 'z'; lower++)
-        putchar(lower);
-
-    for (upper = 'A'; upper <= 'Z'; upper++)
-        putchar(upper);
-
-    putchar('\n');
-
-    return (0);
+	char lower;
+	char upper;
+	
+	for (lower = 'a'; lower <= 'z'; lower++)
+		putchar(lower);
+	
+	for (upper = 'A'; upper <= 'Z'; upper++)
+		putchar(upper);
+	
+	putchar('\n');
+	
+	return (0);
 }
