a5ce4d83c04def5049119841c937c742a9b4083e
tranbnn <tranbn@outlook.com>
2025-06-22 02:54:09 +0000

    fix: editing 5-print

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * Description: main - prints out single digit numbers with a base
+ * of 10 starting from 0
+ * Return: 0 if success
+ */
+int main(void)
+{
+	char num; 
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		printf("%d", num);
+	}
+	putchar('\n');
+	return (0);
+
+}
