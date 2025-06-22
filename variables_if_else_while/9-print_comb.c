33ffc4f9e6feb895f46274d2f5dacce101750034
tranbnn <tranbn@outlook.com>
2025-06-22 04:04:20 +0000

    feat: 9-print

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * Description: main - prints all possible combinations of
+ * single-digit numbers.
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit >= 9; digit++)
+	{
+		putchar((digit % 10) + '0');
+		if (digit == 9)
+			continue;
+		
+		putchar(',');
+		putchar('_');
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
+ 
