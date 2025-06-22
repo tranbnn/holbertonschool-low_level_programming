cf2bb875531ab699d9ef7d512ba25287a152159c
tranbnn <tranbn@outlook.com>
2025-06-22 03:20:42 +0000

    feat: 6-print

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * Description: main - prints all single digit numbers of base 10 starting
+ * from 0, followed by a new line
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int num;
+
+	for(num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
