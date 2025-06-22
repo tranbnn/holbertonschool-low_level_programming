4c3d974f0441234ae90477fa35988f24bc208801
tranbnn <tranbn@outlook.com>
2025-06-22 03:27:37 +0000

    feat: 7-print

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * Description: main - prints alphabet in reverse and lowercase
+ * Return: 0 if success
+ */
+int main (void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
