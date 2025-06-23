0928ac07355b16985b9302485aa7a4cd7a6379f4
tranbnn <tranbn@outlook.com>
2025-06-23 12:20:02 +0000

    feat: adding 0-putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <main.h>
+
+/**
+ * Description: main - prints _putchar, followed by a new line.
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	printf("_putchar\n")
+
+		return (0);
+}
