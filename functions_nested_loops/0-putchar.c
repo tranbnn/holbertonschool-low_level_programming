00acc4b5a0849fccd68172e2bbf4b28069f74cb6
tranbnn <tranbn@outlook.com>
2025-06-23 12:38:36 +0000

    fix: code

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
-#include <main.h>
+#include "main.h"
+#include <stdio.h>
 
 /**
  * Description: main - prints _putchar, followed by a new line.
@@ -7,7 +8,7 @@
 
 int main(void)
 {
-	printf("_putchar\n")
+	printf("_putchar\n");
 
 		return (0);
 }
