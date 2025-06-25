c8a5fa1051d74c3c3a4bcb98f6f4b5818f15f4c0
tranbnn <tranbn@outlook.com>
2025-06-25 13:40:43 +0000

    feat: 2-alphabet created

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * Description: print_alphabet_x10 - prints the alphabet 10 times
+ * 		in lowercase, with a new line.
+ */
+void print_alphabet_x10(void)
+{
+	int line, letter;
+
+	for (line = 0, line <= 9; ++line)
+	{
+		for ( letter = 'a'; letter <= 'z'; ++letter)
+			_putchar(letter);
+		_putchar('\n');
+	}
+}
