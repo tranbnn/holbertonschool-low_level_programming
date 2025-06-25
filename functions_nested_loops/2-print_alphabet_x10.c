df52288c465f47cb5829f3a236c34899f82b343a
tranbnn <tranbn@outlook.com>
2025-06-25 13:41:52 +0000

    fix: code

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,7 @@ void print_alphabet_x10(void)
 {
 	int line, letter;
 
-	for (line = 0, line <= 9; ++line)
+	for (line = 0; line <= 9; ++line)
 	{
 		for ( letter = 'a'; letter <= 'z'; ++letter)
 			_putchar(letter);
