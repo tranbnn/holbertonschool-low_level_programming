e6a4f8ae966c268b68bf3772f851af0750a68669
tranbnn <tranbn@outlook.com>
2025-06-25 15:04:44 +0000

    fix: code

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * Description: _isalpha - checks for alphabetic characters
+ * @c: the character to check
+ * Return: 1 if c is lowercase or 0 if otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (_isalpha(c) == 0)
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
