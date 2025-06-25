da31e57115fc7b4243cc04efd7ee38cc293f910d
tranbnn <tranbn@outlook.com>
2025-06-25 14:27:32 +0000

    fix: code

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * Description: _islower - checks for lowercase characters
+ * Return: 1 if lowercase or 0 if otherwise
+ */
+
+int _islower(int c)
+{
+	int c;
+
+	if (_islower(c))
+	{
+		_putchar(c);
+		return (1);
+	}
+	else
+	{
+		_putchar(c);
+		return (0);
+	}
+}
