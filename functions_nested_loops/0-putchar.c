7e00d056b13b7813060cfb40bc216d6a6656a4b9
tranbnn <tranbn@outlook.com>
2025-06-25 12:06:33 +0000

    feat: new directory

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * Description: main - prints _putchar with a new line
+ * Return: 0 if success
+ */
+
+int main (void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
+
