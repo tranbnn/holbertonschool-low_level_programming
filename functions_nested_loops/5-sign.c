279408a391abe376c5e509ba1f42eda4055d8eed
tranbnn <tranbn@outlook.com>
2025-06-29 05:21:14 +0000

    feat: signs

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * Description: print_sign - prints the sign of a number
+ * Return: 1 if greater than 0, 0 if is zero, or -1 if less than 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else 
+	{
+		_putchar('0');
+		return (0);
+	}
+
+}
