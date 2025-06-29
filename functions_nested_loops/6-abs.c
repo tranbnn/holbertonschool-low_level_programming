e64dc25b92cc7ecf87c22ba328041bff43b95c9d
tranbnn <tranbn@outlook.com>
2025-06-29 08:28:37 +0000

    fix: main doc

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * Description: _abs - computes absolute value of an integer
+ * @n: the integer to check
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
