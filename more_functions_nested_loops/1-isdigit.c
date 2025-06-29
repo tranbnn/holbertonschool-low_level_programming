da1a9f527f322c7e662b7823f74d6bdb72e92b90
tranbnn <tranbn@outlook.com>
2025-06-29 15:39:45 +0000

    feat: 1-isdigit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * Description: _isdigit - checks for a digit
+ * @c: int to look for
+ * Return: 1 if c is a digit, 0 if otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+}
