f6927f50d289bd1aba261a5ff0c10af8a1ee4cd6
tranbnn <tranbn@outlook.com>
2025-06-29 08:44:48 +0000

    fix: code

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,12 +2,13 @@
 /**
  * Description: _abs - computes absolute value of an integer
  * @n: the integer to check
+ * Return: The absolute value of the integer
  */
 int _abs(int n)
 {
 	if (n >= 0)
 	{
-		return (n);
+		return(n);
 	}
 	return (-n);
 }
