e2255d68a2dc883edd262052e44ea5fbbeacd369
tranbnn <tranbn@outlook.com>
2025-06-29 09:36:28 +0000

    fix: forgot ;

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/** 
+ * Description: jack_bauer - prints every minute fo the day
+ * of Jack Bauer, starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int hour, min;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hour/10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(';');
+			_putchar((min/10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}	
+	}
+}
