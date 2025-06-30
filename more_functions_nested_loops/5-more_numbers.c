b687635966473dd85795b66293505c301cf1196e
tranbnn <tranbn@outlook.com>
2025-06-30 12:12:20 +0000

    fix: code

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,15 @@ void more_numbers(void)
 {
 	int times, n;
 
-	for (times = 1; times <= 10; ++times)
+	for (times = 0; times <= 10; times++)
 	{
-		for (n = 0; n <= 14; ++times)
+		for (n = 0; n <= 14; times++)
 		{
-			if (n > 9)
+			if (n >= 10)
 			{
-				_putchar((n / 10) + '0');
+				_putchar(n / 10 + '0');
 			}
-
-			_putchar((n % 10) + '0');
+			_putchar(n % 10 + '0');
 		}
 		_putchar('\n');
 	}
