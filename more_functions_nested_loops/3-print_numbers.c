ae9529d58ac5705e5714e4e2f88ab6dc395c27ca
tranbnn <tranbn@outlook.com>
2025-06-30 09:18:23 +0000

    feat: 3-print

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * Description: print_numbers - print numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	int n;
+	
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
