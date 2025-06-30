26745a048a319ca0615e5db6f470cd89d3c39996
tranbnn <tranbn@outlook.com>
2025-06-30 15:14:41 +0000

    feat: triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * Description: print_triangle - prints a triangle
+ * @size: is the size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int y, x; 
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (y = 1; y <= size; y++)
+		{
+			for (x = 1; x <= size; x++)
+			{
+				if ((y + x) <= size)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar ('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
