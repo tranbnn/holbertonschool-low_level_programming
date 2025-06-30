bb723900e6891a1c3e5244d65ec2a5e9115781c7
tranbnn <tranbn@outlook.com>
2025-06-30 09:52:45 +0000

    fix: <=

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * Description: print_most_numbers - prints numbers from 0 to 9, 
+ * Description: print_most_numbers - prints numbers from 0 to 9,
  * excluding 2 and 4
  */
 void print_most_numbers(void)
