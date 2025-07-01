d16660f5fd409f8319fbbc5f51668c0f40e33349
tranbnn <tranbn@outlook.com>
2025-07-01 14:01:37 +0000

    fix: code

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,8 +1,15 @@
 #include "main.h"
 /**
  * Description: _puts - prints a string to stdout
+ * @str: the string to be printed
  */
 void _puts(char *str)
 {
-	_putchar(*str);
+	int s;
+
+	for (s = 0; str[s] != '\0'; s++)
+	{
+		_putchar(str[s]);
+	}
+	_putchar('\n');
 }
