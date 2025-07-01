b6363cf2e5e1f7b55b3cb2a6f49f09bde1609874
tranbnn <tranbn@outlook.com>
2025-07-01 13:29:48 +0000

    feat: 1-swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * Description: swap_int - swaps values of two integers
+ * @a: first int to have value swapped
+ * @b: second int to have value swapped
+ */
+void swap_int(int *a, int *b)
+{
+	printf("a = %d, b = %d\n", *a, *b);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -15,5 +15,6 @@ void reset_to_98(int *n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+void swap_int(int *a, int *b);
 
 #endif
