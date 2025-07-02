109790bb80791054a3a86a01c5a130821e8c1a2e
tranbnn <tranbn@outlook.com>
2025-07-02 09:06:37 +0000

    feat: 5-rev

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * Description: rev_string - reverses a string
+ * @s: the string to be reversed.
+ */
+void rev_string(char *s)
+{
+	int count = 0;
+	int length; 
+	char temp;
+
+	while (s[count] != '\0')
+	{
+		count = count + 1;
+	}
+
+	count = count - 1;
+	length = count;
+
+	while (count > (length / 2))
+	{
+		temp = s[count];
+		s[count] = s[length - count];
+		s[length - count] = temp;
+		
+		count = count - 1;	
+	}
+}
