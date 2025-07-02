bd5f32be3def6833ac5380b523fd1521944784ad
tranbnn <tranbn@outlook.com>
2025-07-02 09:39:02 +0000

    feat: 6-puts

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * Description: puts2 - prints every other character of a string
+ * @str: the string to be printed.
+ * Return: string with missing characters
+ */
+void puts2(char *str)
+{
+	int count = 0;
+
+	while (str[count] !- '\0')
+	{
+		if (count % 2 == 0)
+		{
+			_putchar(str[count]);
+		}
+
+		count = count + 1;
+	}
+	_putchar('\n');
+}
