2a892d7636ff05740f8d6adf6fea7569b6d22bb4
tranbnn <tranbn@outlook.com>
2025-07-02 11:10:39 +0000

    feat: 9-strcpy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * Description: *_strcpy - copies the string pointed to by src,
+ * including the terminating null byte \0 to the buffer
+ * pointed by dest
+ * @dest: destination
+ * @src: source
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a, b = 0;
+	
+	for (a = 0; src[a] != '\0'; ++a)
+	{
+		dest[b] = src[a];
+		++b;
+	}
+	dest[b] = '\0';
+
+	return (dest);
+}
