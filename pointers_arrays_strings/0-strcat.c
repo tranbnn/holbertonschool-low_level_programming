f6f207b9cf3458e0d45c89376f6925ed56562d4f
tranbnn <tranbn@outlook.com>
2025-07-07 12:36:07 +0000

    fix: code

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * Description: *_strcat - concatenates two strings
+ * @dest: the string src is to be appended to
+ * @src: the string to be appended to dest
+ * Returns: a pointer to the resulting string @dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int s, t;
+
+	s = 0;
+	t = 0;
+
+	while (dest[s] != '\0')
+	{
+		s++;
+	}
+
+	while (src[t] != '\0')
+	{
+		dest[s] = src[t];
+		t++;
+		s++;
+	}
+
+	dest[s] = '\0';
+
+	return (dest);
+}
