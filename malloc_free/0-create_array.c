f268c718fd84b9ccd9923a91f5e11ce321e77428
tranbnn <tranbn@outlook.com>
2025-07-07 11:42:08 +0000

    feat: 0-create

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * Description: *create_array - creates an array of chars,
+ * and initialises it with a specific char.
+ * @size: int that represents size of array
+ * @c: char that will initialise array
+ * Return: NULL if size = 0, or a pointer to the array.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+}
