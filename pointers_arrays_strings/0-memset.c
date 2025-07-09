e601fb95b468ada6e34d934e9208d70a501d1302
tranbnn <tranbn@outlook.com>
2025-07-09 10:19:31 +0000

    0-memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * Description: _memset - fills memory with a constant byte
+ * @n: input integer
+ * @s: input string
+ * @b: input string
+ * Returns: pointer to @s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i = i + 1;
+	}
+	return (s);
+}
