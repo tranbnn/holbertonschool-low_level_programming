f521ffefebbdb9635a86521d7fa8412bb3cfabcf
tranbnn <tranbn@outlook.com>
2025-07-09 10:46:45 +0000

    feat: 2-str

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * Description: _strchr - locates a character in a string
+ * @s: string to be searched
+ * @c: character to be found
+ * Return: pointer to first occurence of character @c OR
+ * null if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+	
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+		i = i + 1;
+	}
+
+	return ('\0');
+}
