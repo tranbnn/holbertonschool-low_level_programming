40bbb746c95597745cee96f986a0523dc718d7a4
tranbnn <tranbn@outlook.com>
2025-07-10 04:48:24 +0000

    fix: strange error

---
+++ recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * Description: factorial - returns the factorial of a given number
+ * @n: the given number
+ * Return: the factorial or -1 to indicate an error
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return( n * factorial(n - 1));
+	}
+}
