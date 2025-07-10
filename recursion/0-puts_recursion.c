e14e9cb7c0218dccb29469abffce9e10ed201532
tranbnn <tranbn@outlook.com>
2025-07-10 03:34:05 +0000

    feat: 0-puts

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * Description: _puts_recursion - prints a string
+ * @s: the string to be printed
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	return ();
+}
