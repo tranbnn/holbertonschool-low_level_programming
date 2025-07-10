ab5ed655b3e0e672bb5475c34edcf5be2d318e8b
tranbnn <tranbn@outlook.com>
2025-07-10 03:49:07 +0000

    feat

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -7,6 +7,16 @@
 
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	return;
+	int i;
+
+	i = 0;
+
+	if (s[i] == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(s[i]);
+	_puts_recursion(s + 1);
 }
