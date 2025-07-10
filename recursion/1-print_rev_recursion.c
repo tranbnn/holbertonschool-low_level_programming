5e62a70f2e8183abd2634e31049a7c3ab9634ca0
tranbnn <tranbn@outlook.com>
2025-07-10 04:11:20 +0000

    feat: 1-print

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * Description: _print_rev_recursions - prints a string in reverse
+ * @s: the string to be reversed
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+
+	if (s[i] == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(s[i]);
+}
