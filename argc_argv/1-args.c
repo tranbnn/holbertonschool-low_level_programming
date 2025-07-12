0620d0c04e2188cb740930746cf29900f8af113b
tranbnn <tranbn@outlook.com>
2025-07-12 13:16:27 +0000

    1-args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * Description: main - prints the number of arguments passed into it
+ * @argc: the array of arguments
+ * @argv: the array of strings
+ * Return: the number of arguments
+ */
+
+int main(int argc, char *argv[] __attribute__ ((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
