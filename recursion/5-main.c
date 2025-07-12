d7453137a2522800586912518df8cddc3069e318
tranbnn <tranbn@outlook.com>
2025-07-12 04:34:41 +0000

    fix

---
+++ recursion/5-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * Description: _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to find the square root of
+ * Returns: the square root of @n or -1
+ */
+
+int _sqrt_recursion(int n)
+{
+	
+}
