d5e2d32dc5be1824243836294fc4b0091607d4bf
tranbnn <tranbn@outlook.com>
2025-07-12 10:47:13 +0000

    0-whats

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * Description: main - prints the program's name
+ * Return: the program's name
+ */
+
+int main (int argc, char* argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", *argv);
+	}
+	return (0);
+}
