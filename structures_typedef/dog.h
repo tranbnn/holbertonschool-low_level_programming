ca9a10a7c759dcce2260aa48a01b4d16d3fad060
tranbnn <tranbn@outlook.com>
2025-07-15 09:31:28 +0000

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+
+/**
+ * struct dog - information of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: information of a dog, including name,
+ * age, and owner.
+ */
+
+struct dog
+{
+	char * name;
+	float age;
+	char * owner;
+};
+
+#endif
