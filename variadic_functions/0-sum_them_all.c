e45934bc3b6c1e87a4dbeaf90e0c5516b05e218d
tranbnn <tranbn@outlook.com>
2025-07-17 04:59:22 +0000

    0-sum_them_all.c

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,16 @@
+#include "variadic_functions.h"
+/**
+ * Description: sum_them_all - returns the sum of all its parameters
+ * @n: a constant value that is to be added with other parameters
+ * Return: the resulting sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	
+	if (n == 0)
+	{
+		return (0);
+	}
+}
