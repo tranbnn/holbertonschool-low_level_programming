95fccaf73bd1784651c8ff59b6e98d65d66a36ee
tranbnn <tranbn@outlook.com>
2025-07-17 06:02:31 +0000

    0-sum_them_all.c

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -8,9 +8,24 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
-	
+	unsigned int sum;
+	va_list args;
+
 	if (n == 0)
 	{
 		return (0);
 	}
+
+	i = 0;
+	
+	va_start(args, n);
+	
+	while (i < n)
+	{
+		sum += va_arg(args, unsigned int);
+		i++;
+	}
+
+	va_end(args);
+	return (sum);
 }
