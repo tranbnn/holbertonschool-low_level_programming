919028bed62da83ff2ab13d907c24e39a293eb09
tranbnn <tranbn@outlook.com>
2025-07-20 07:23:00 +0000

    variadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -9,5 +9,9 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_a_char(va_list args);
+void print_an_int(va_list args);
+void print_a_float(va_list args);
+void print_a_str(va_list args);
 
 #endif
