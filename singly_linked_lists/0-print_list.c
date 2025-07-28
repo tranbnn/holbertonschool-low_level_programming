dcac9a8f69c98714914ee7ff3d26eff3ef07602c
tranbnn <tranbn@outlook.com>
2025-07-28 12:47:56 +0000

    single lls

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a list_t list
+ * @h: pointer to the list
+ * Return: the number of nodes or [0] (nil)
+ */
+
+size_t print_list(const list_t *h)
+{
+
+}
