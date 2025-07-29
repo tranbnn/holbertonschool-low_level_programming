700f2fdc08ee98108cc64616ceeef4147d97eaa9
tranbnn <tranbn@outlook.com>
2025-07-29 13:12:00 +0000

    task 1

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in a linked list
+ * @h: pointer to the list
+ * Return: the number of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
