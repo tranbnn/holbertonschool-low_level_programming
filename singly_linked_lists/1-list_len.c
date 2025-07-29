3967761cb15dba00a334af1bf5a14b20a8ee8e38
tranbnn <tranbn@outlook.com>
2025-07-29 08:48:13 +0000

    task 1

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -5,7 +5,7 @@
 /**
  * list_len - returns the number of elements in a linked list
  * @h: pointer to the list
- * Returns: the number of elements in the list
+ * Return: the number of elements in the list
  */
 
 size_t list_len(const list_t *h)
