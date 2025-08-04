3e28f68ec1f7576924e3526c1b6d09c19520aa4f
tranbnn <tranbn@outlook.com>
2025-08-04 13:26:41 +0000

    task 3

---
+++ file_io/3-cp.c
@@ -0,0 +1,70 @@
+#include "main.h"
+/**
+ * main - copies file content to another
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 1 on success or -1 on failure
+ */
+
+int main(int argc, char *argv[])
+{
+	int to = 0;
+	int from = 0;
+	int to_count = 0;
+	char buffer[1024];
+	int from_count = 1024;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO,
+				"Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	from = open(argv[1], O_RDONLY);
+	if (from == 1)
+	{
+		dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (to == 1)
+	{
+		dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	while (from_count == 1024)
+	{
+		from_count = read(from, buffer, 1024);
+		if (from_count == -1)
+		{
+			dprintf(STDERR_FILENO,
+					"Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		to_count = write(to, buffer, from_count);
+		if (to_count == -1)
+		{
+			dprintf(STDERR_FILENO,
+					"Error: Can't wait to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+
+	if (close(from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
+		exit(100);
+	}
+	if (close(to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to);
+		exit(100);
+	}
+
+	return (0);
+}
--- file_io/main.h
+++ file_io/main.h
@@ -6,7 +6,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-
+#include <stdio.h>
 int _putchar(char c);
 
 ssize_t read_textfile(const char *filename, size_t letters);
