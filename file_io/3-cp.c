0e89f15b61d0b364fbb9f80784e6550719e54433
tranbnn <tranbn@outlook.com>
2025-08-04 14:10:22 +0000

    task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -51,7 +51,7 @@ int main(int argc, char *argv[])
 	if (from_count == -1)
 	{
 		dprintf(STDERR_FILENO,
-				"Error: Can't read from file %\n", argv[1]);
+				"Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 
 	}
