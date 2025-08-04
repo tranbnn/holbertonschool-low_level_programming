6f8301219e4a748f1122faf21f722829c7d82f00
tranbnn <tranbn@outlook.com>
2025-08-04 12:22:23 +0000

    task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -38,7 +38,7 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 
-	output = write (fd, text_content, count);
+	output = write(fd, text_content, count);
 	if (output == -1 || output != count)
 	{
 		return (-1);
