beac3b8e9352a82961952f10ca6979cdae08e8fc
tranbnn <tranbn@outlook.com>
2025-08-05 14:52:06 +0000

    6

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -11,22 +11,22 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int index;
 	hash_node_t *node, *daum;
 
-	index = 0;
-	node = NULL;
-	daum = NULL;
+	if (ht == NULL || ht->array == NULL)
+	{
+		return;
+	}
 	
-	while (index < ht->size)
+	for (index = 0; index < ht->size; index++)
 	{
 		node = ht->array[index];
 		while (node)
 		{
 			daum = node->next;
-			free(node->value);
 			free(node->key);
+			free(node->value);
 			free(node);
 			node = daum;
 		}
-		index++;
 	}
 	free(ht->array);
 	free(ht);
