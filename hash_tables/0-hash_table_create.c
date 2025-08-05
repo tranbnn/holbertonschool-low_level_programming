8c8253909bffbb26e3f31590ac88becfa7fab7d1
tranbnn <tranbn@outlook.com>
2025-08-05 09:47:00 +0000

    0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,12 +10,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
 
+	table = malloc(sizeof(hash_table_t) * size);
+
 	if (table == NULL)
 	{
 		return (NULL);
 	}
-
-	table = malloc(sizeof(hash_table_t) * size);
 
 	return (table);
 }
