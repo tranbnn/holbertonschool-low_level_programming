a42202211716fd08fc368c3dc9fcff03ad3ba785
tranbnn <tranbn@outlook.com>
2025-08-05 13:13:14 +0000

    3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -29,12 +29,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 
 	node->key = strdup(key);
-	node->value = strdup (value);
-	node->next =NULL;
+	node->value = strdup(value);
+	node->next = NULL;
 
 	if (ht->array[index] == NULL)
 	{
-		ht->array[index] = node; 
+		ht->array[index] = node;
 	}
 	else
 	{
